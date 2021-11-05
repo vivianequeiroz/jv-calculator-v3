c12505f82743a78b4f1a08315debe804154e0cd1
João Bispo <joaobispo2077@gmail.com>
2021-11-04 21:43:05 -0300

    feat: add comma validation

---
+++ src/utils/calculator.ts
@@ -0,0 +1,90 @@
+// export
+type MathExpressionSymbols = 'x' | '/' | '+' | '-';
+
+const expressionSymbols = ['+', '-', 'x', '/', ','];
+
+function getOperationBySymbol(symbol: MathExpressionSymbols) {
+  // type MathOperation = (a: number, b: number) => number;
+
+  return (a: number, b: number) => `testing...${a} ${b}`;
+}
+
+function calculate(expression: string): string {
+  const satinizedExpression = expression.replace(/,/, '.').replace(/,/, '');
+
+  const foundFirstSymbol = Array.from(satinizedExpression).find(
+    (symbol, index) => expressionSymbols.includes(symbol) && index !== 0,
+  );
+
+  const mathOperate = getOperationBySymbol(
+    foundFirstSymbol as MathExpressionSymbols,
+  );
+
+  const [firstNumber, secondNumber] = satinizedExpression.split(
+    foundFirstSymbol as MathExpressionSymbols,
+  );
+
+  const result = mathOperate(Number(firstNumber), Number(secondNumber));
+
+  return result.toString().replace('.', ',');
+}
+
+const isValidCommaExpression = (expression: string): boolean => {
+  const splittedExpression = expression.split(/-|\+|x|\//g);
+
+  if (expression.includes(',')) {
+    const hasCommaQuantityByIndexList = splittedExpression.map((value) =>
+      value.match(/,/g) ? value.match(/,/g)?.length : 0,
+    );
+
+    return hasCommaQuantityByIndexList.every(
+      (value) => value !== undefined && value <= 1,
+    );
+  }
+
+  return true;
+};
+
+const isValidExpression = (
+  expression: string,
+  candidateText: string,
+): boolean => {
+  const operationsSymbols = ['+', '-', 'x', '/'];
+
+  const lastValue = expression.slice(-1);
+
+  const isCandidateRepeatSymbol =
+    expressionSymbols.includes(lastValue) &&
+    expressionSymbols.includes(candidateText);
+
+  const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
+  const isValidExpression =
+    validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
+
+  const invalidSymbolsAsFirstValue = ['x', '/', ','];
+
+  const isValidFirstValue =
+    isValidExpression && !invalidSymbolsAsFirstValue.includes(candidateText);
+
+  const isEmptyExpression = expression === '';
+  if (isEmptyExpression) {
+    return isValidFirstValue;
+  }
+
+  const hasInvalidTwoFactors =
+    !!Array.from(expression).find(
+      (expressionValue, index) =>
+        operationsSymbols.includes(expressionValue) && index !== 0,
+    ) && operationsSymbols.includes(candidateText);
+
+  const isValidComma = isValidCommaExpression(`${expression}${candidateText}`);
+
+  return !!isValidExpression && !hasInvalidTwoFactors && isValidComma;
+};
+
+export {
+  calculate,
+  getOperationBySymbol,
+  expressionSymbols,
+  isValidExpression,
+};
