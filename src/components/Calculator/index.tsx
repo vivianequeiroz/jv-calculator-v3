264e62aa427b2014dd040d0df450b02cfeee49e0
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-11-04 22:30:00 -0300

    chore: add of calculator title

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -2,77 +2,80 @@ import './styles.scss';
 
 export function Calculator() {
   return (
-    <form className="calculator-container">
-      <input className="input-numbers" />
-      <div className="buttons">
-        <div className="buttons-operators">
-          <div className="button-column">
-            <button type="button" className="button">
-              7
-            </button>
-            <button type="button" className="button">
-              4
-            </button>
-            <button type="button" className="button">
-              1
-            </button>
-            <button type="button" className="button">
-              ,
-            </button>
+    <div>
+      <h1 className="title">JV Calculator 3</h1>
+      <form className="calculator-container">
+        <input className="input-numbers" />
+        <div className="buttons">
+          <div className="buttons-operators">
+            <div className="button-column">
+              <button type="button" className="button">
+                7
+              </button>
+              <button type="button" className="button">
+                4
+              </button>
+              <button type="button" className="button">
+                1
+              </button>
+              <button type="button" className="button">
+                ,
+              </button>
+            </div>
+            <div className="button-column">
+              <button type="button" className="button">
+                8
+              </button>
+              <button type="button" className="button">
+                5
+              </button>
+              <button type="button" className="button">
+                2
+              </button>
+              <button type="button" className="button">
+                0
+              </button>
+            </div>
+            <div className="button-column">
+              <button type="button" className="button">
+                9
+              </button>
+              <button type="button" className="button">
+                6
+              </button>
+              <button type="button" className="button">
+                3
+              </button>
+              <button className="button" type="button">
+                /
+              </button>
+            </div>
+            <div className="button-column">
+              <button type="button" className="button del">
+                DEL
+              </button>
+              <button className="button" type="button">
+                +
+              </button>
+              <button className="button" type="button">
+                -
+              </button>
+              <button className="button" type="button">
+                x
+              </button>
+            </div>
           </div>
-          <div className="button-column">
-            <button type="button" className="button">
-              8
+          <div className="button-controllers">
+            <button type="button" className="reset">
+              RESET
             </button>
-            <button type="button" className="button">
-              5
-            </button>
-            <button type="button" className="button">
-              2
-            </button>
-            <button type="button" className="button">
-              0
-            </button>
-          </div>
-          <div className="button-column">
-            <button type="button" className="button">
-              9
-            </button>
-            <button type="button" className="button">
-              6
-            </button>
-            <button type="button" className="button">
-              3
-            </button>
-            <button className="button" type="button">
-              /
-            </button>
-          </div>
-          <div className="button-column">
-            <button type="button" className="button del">
-              DEL
-            </button>
-            <button className="button" type="button">
-              +
-            </button>
-            <button className="button" type="button">
-              -
-            </button>
-            <button className="button" type="button">
-              x
+            <button type="submit" className="result">
+              =
             </button>
           </div>
         </div>
-        <div className="button-controllers">
-          <button type="button" className="reset">
-            RESET
-          </button>
-          <button type="submit" className="result">
-            =
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
