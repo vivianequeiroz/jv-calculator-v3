9eb83044f9dfa601f8d9c9f67a2770b6461b5ffe
João Bispo <joaobispo2077@gmail.com>
2021-11-07 16:54:10 -0300

    test: should be able to calculate operation sum between two numbers

--- src/utils/__tests__/calculator.spec.ts
+++ src/utils/__tests__/calculator.spec.ts
@@ -1,4 +1,4 @@
-import { isValidExpression } from '../calculator';
+import { isValidExpression, calculate } from '../calculator';
 
 describe('Calculator utils', () => {
   describe('expression validation', () => {
@@ -22,6 +22,17 @@ describe('Calculator utils', () => {
     it('should be able to return false when received a invalid expression with semicolon', () => {
       expect(isValidExpression('-10,1', ',')).toBe(false);
       expect(isValidExpression('-10,1,2/', '2,0')).toBe(false);
+    });
+  });
+
+  describe('calculate math expression', () => {
+    it('should be able to calculate operation sum between two numbers', () => {
+      expect(calculate('1+1')).toBe('2');
+      expect(calculate('1,1+1,2')).toBe('2,3');
+      expect(calculate('2+2')).toBe('4');
+      expect(calculate('3+2')).toBe('5');
+      expect(calculate('37,5+28,2')).toBe('65,7');
+      expect(calculate('90+10')).toBe('100');
     });
   });
 });
