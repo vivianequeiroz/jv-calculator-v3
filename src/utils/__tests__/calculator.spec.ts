77a28e16f65a82cb7d4d8469753c8845a8032de5
João Bispo <joaobispo2077@gmail.com>
2021-11-07 16:39:48 -0300

    test: remove calculator obj to use isValidExpression

--- src/utils/__tests__/calculator.spec.ts
+++ src/utils/__tests__/calculator.spec.ts
@@ -1,27 +1,27 @@
-import * as calculator from '../calculator';
+import { isValidExpression } from '../calculator';
 
 describe('Calculator utils', () => {
   describe('expression validation', () => {
     it('should be able to return true when received a simple valid expression', () => {
-      expect(calculator.isValidExpression('1+', '1')).toBe(true);
-      expect(calculator.isValidExpression('-1*', '1')).toBe(true);
-      expect(calculator.isValidExpression('13543/', '1')).toBe(true);
-      expect(calculator.isValidExpression('13543-', '1')).toBe(true);
+      expect(isValidExpression('1+', '1')).toBe(true);
+      expect(isValidExpression('-1*', '1')).toBe(true);
+      expect(isValidExpression('13543/', '1')).toBe(true);
+      expect(isValidExpression('13543-', '1')).toBe(true);
     });
 
     it('should be able to return true when received a valid expression with semicolon', () => {
-      expect(calculator.isValidExpression('-10,15+', '1')).toBe(true);
-      expect(calculator.isValidExpression('-10,15-', '21,5')).toBe(true);
-      expect(calculator.isValidExpression('-10,15-', '21,5')).toBe(true);
+      expect(isValidExpression('-10,15+', '1')).toBe(true);
+      expect(isValidExpression('-10,15-', '21,5')).toBe(true);
+      expect(isValidExpression('-10,15-', '21,5')).toBe(true);
     });
 
     it('should be able to return false when received a simple invalid expression', () => {
-      expect(calculator.isValidExpression('1+', '-')).toBe(false);
+      expect(isValidExpression('1+', '-')).toBe(false);
     });
 
     it('should be able to return false when received a invalid expression with semicolon', () => {
-      expect(calculator.isValidExpression('-10,1', ',')).toBe(false);
-      expect(calculator.isValidExpression('-10,1,2/', '2,0')).toBe(false);
+      expect(isValidExpression('-10,1', ',')).toBe(false);
+      expect(isValidExpression('-10,1,2/', '2,0')).toBe(false);
     });
   });
 });
