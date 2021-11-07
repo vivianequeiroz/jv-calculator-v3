b914cafc774a6d51787a2b3a07639646bcbb5130
João Bispo <joaobispo2077@gmail.com>
2021-11-07 16:36:49 -0300

    feat: add module argumentation from get latest dweet for

--- src/@types/dweetio.d.ts
+++ src/@types/dweetio.d.ts
@@ -1,6 +1,10 @@
 import { DweetDTO } from '../dtos/DweetDTO';
 
 export type Dweetio = {
+  get_latest_dweet_for: (
+    thing: string,
+    callback: (err: Error | null, dweet: DweetDTO[]) => void,
+  ) => void;
   listen_for: (thing: string, callback: (dweet: DweetDTO) => void) => void;
   stop_listening_for: (thing: string) => void;
 };
